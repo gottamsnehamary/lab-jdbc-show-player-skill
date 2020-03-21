447f35eac849a89499cb49a7ee42db43cdf02498
gottamsnehamary <gottamsneha21@gmail.com>
2020-03-21 22:43:15 +0530

    lab-jdbc-show-player-skill

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,27 @@
+package controller;
 
+import java.util.ArrayList;
+
+import dao.SkillDAO;
+import model.Skill;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception 
+	{
+	
+	SkillDAO skilldao=new SkillDAO();
+			
+ArrayList<Skill>  list= (ArrayList<Skill>) skilldao.listAllSkills();
+
+
+System.out.println("Id"+"\t"+"name");
+
+for(Skill s: list)
+{
+	System.out.println(s.getSkilld()+"\t"+s.getSkillName());
+
+}
+	}
+
+}
--- src/dao/SkillDAO.java
+++ src/dao/SkillDAO.java
@@ -1,1 +1,42 @@
+package dao;
 
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Skill;
+import utility.ConnectionManager;
+
+public class SkillDAO
+{
+	static List<Skill> list=new ArrayList<Skill>();
+	public List< Skill > listAllSkills() throws Exception
+	{
+        Connection con = ConnectionManager.getConnection();
+        
+		Statement stmt=  con.createStatement();
+		
+		ResultSet rs =stmt.executeQuery("select * from SKILL order by name asc ");
+		
+		while(rs.next())
+		{
+			
+			long id=rs.getLong(1);
+			String name=rs.getString(2);
+			Skill skill=new Skill(id,name);
+			
+			list.add(skill);
+			
+		}
+		
+		return list;
+		
+		
+	}
+	
+	
+
+	
+}
